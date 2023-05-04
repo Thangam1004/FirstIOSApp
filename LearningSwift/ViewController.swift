3485a1c71849de561667e3a0dc9be676fc5ce68a
Thangam1004 <131429112+Thangam1004@users.noreply.github.com>
2023-05-04 18:23:47 +0530

    Add files via upload
    done GST calculator

---
+++ LearningSwift/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  LearningSwift
+//
+//  Created by Thangam on 26/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var originalPriceValue: Float = 0
+    var gstPercentageValue: Float = 0
+
+    @IBOutlet weak var afterGstTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+    
+    @IBAction func originalPriceChanged(_ sender: UITextField) {
+        if let value = Float(sender.text!) {
+            originalPriceValue = value
+        } else {
+            originalPriceValue = 0
+        }
+        afterGstTextField.text?.removeAll()
+    }
+    
+    @IBAction func gstPercentageChanged(_ sender: UITextField) {
+        if let value = Float(sender.text!) {
+            gstPercentageValue = value
+        } else {
+            gstPercentageValue = 0
+        }
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        
+        let gstValue = originalPriceValue * gstPercentageValue / 100
+        let priceWithGst = originalPriceValue + gstValue
+        
+        afterGstTextField.text = String(priceWithGst)
+    }
+    @IBAction func websiteButton(_ sender: UIButton) {
+        if let url = URL(string: "https://www.google.com") {
+            UIApplication.shared.open(url)
+        }
+    }
+}
